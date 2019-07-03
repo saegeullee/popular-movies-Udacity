102711ff261d91e4fc456c32b21d7be968b8d576
saegeullee <lemix777@naver.com>
2019-07-03 16:47:29 +0900

    getMoviePost method completed

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends AppCompatActivity
                 public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
 
                     List<Movie> movies = response.body().getMovies();
+                    mAdapter.setPosterListData(movies);
 
                     if(movies != null)
                         Log.d(TAG, "onResponse: movies : " + movies.toString());
@@ -92,6 +93,7 @@ public class MainActivity extends AppCompatActivity
                 @Override
                 public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
                     List<Movie> movies = response.body().getMovies();
+                    mAdapter.setPosterListData(movies);
 
                     if(movies != null)
                         Log.d(TAG, "onResponse: movies : " + movies.toString());
@@ -143,63 +145,4 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-//    public class getMovieAsyncTask extends AsyncTask<String, Void, List<Movie>> {
-//
-//        /**
-//         * TODO question1
-//         * when should I leave or delete super. method ?
-//         * how do I know it ?
-//         */
-//
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected List<Movie> doInBackground(String... strings) {
-//
-//            Log.d(TAG, "doInBackground: in");
-//
-//            String queryMethod = strings[0];
-//            Log.d(TAG, "doInBackground: queryMethod : " + queryMethod);
-//
-//            if(queryMethod.equals("popular")) {
-//                try {
-//                    String jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(Constants.MOVIE_POPULAR);
-//                    Log.d(TAG, "doInBackground: jsonMovieResponse + " + jsonMovieResponse);
-//
-//                    posterList = MoviesApiJsonUtils.getPosterListFromJson(jsonMovieResponse);
-//
-//                    return posterList;
-//
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            } else if(queryMethod.equals("top_rated")) {
-//                try {
-//                    String jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(Constants.MOVIE_TOP_RATED);
-//                    Log.d(TAG, "doInBackground: jsonMovieResponse + " + jsonMovieResponse);
-//
-//                    posterList = MoviesApiJsonUtils.getPosterListFromJson(jsonMovieResponse);
-//
-//                    return posterList;
-//
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(List<Movie> posters) {
-//
-//            Log.d(TAG, "onPostExecute: in");
-//
-//            if(posters != null)
-//                mAdapter.setPosterListData(posters);
-//        }
-//    }
 }
